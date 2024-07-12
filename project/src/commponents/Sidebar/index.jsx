6e9a76ccd24f2d29ebf3d2eaf5151ed7a596e9b3
jmctheowl8 <john.cabrera@lc.cuny.edu>
2024-07-12 19:18:57 -0400

    testing commit

--- project/src/commponents/Sidebar/index.jsx
+++ project/src/commponents/Sidebar/index.jsx
@@ -1,30 +1,59 @@
-
-import{Link, NavLink} from 'react-router-dom'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import './index.scss'
+import { useState } from 'react'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-import './index.scss'
-const Sidebar = () =>(
-    <div className='nav-bar'>
-        <Link className="logo" to="/"> 
-            <img src={LogoS} alt="logo"/>
-            <img className="sub_logo" src={LogoSubtitle} alt="slobodan"/>
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-            </NavLink>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
-            <NavLink exact="true" activeclassname="active" className='about-link' to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-            </NavLink>
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
 
-            <NavLink exact="true" activeclassname="active" className='contact-link' to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-            </NavLink>
-        </nav>
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink exact="true" activeclassname="active" to="/" onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active"className="about-link" to="/about" onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon' />
+      </nav>
     </div>
-)
 
-export default Sidebar
+  )
+}
+
+export default Sidebar;
