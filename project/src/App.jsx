da39f41c0f6085034061b08c58531f77f0d55c86
jmctheowl8 <john.cabrera@lc.cuny.edu>
2024-07-12 12:34:21 -0400

    save

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './commponents/Layout'
+import Home from './commponents/Home'
 
 function App() {
   return (
     <>
      <Routes>
       <Route path="/" element={<Layout />} />
+       <Route index element={<Home />} />
      </Routes>
     </>
   )
