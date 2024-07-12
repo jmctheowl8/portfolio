8246e4c54349b923283990dfbb88fedd9dc0d770
jmctheowl8 <john.cabrera@lc.cuny.edu>
2024-07-12 12:41:41 -0400

    "Moved import statement for index.scss in sidebar/index.jsx and added styles for sidebar navigation"

--- project/src/commponents/sidebar/index.jsx
+++ project/src/commponents/sidebar/index.jsx
@@ -1,9 +1,10 @@
-import './index.scss'
+
 import{Link, NavLink} from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import './index.scss'
 const Sidebar = () =>(
     <div className='nav-bar'>
         <Link className="logo" to="/"> 
