053e2dbbee8b8bdcf41750ba607f88cb73ada84a
jmctheowl8 <john.cabrera@lc.cuny.edu>
2024-08-02 17:10:24 -0400

    added comma

--- project/src/commponents/Skills/TextSection/index.jsx
+++ project/src/commponents/Skills/TextSection/index.jsx
@@ -6,7 +6,7 @@ const TextSection = () => {
             <h1>Skills</h1>
             <p>Experince working with front end development including technolgies such as HTML5, css3, javascript, React, Bootstrap, tailwind, Sass, Git. Also have experince in backend technologies node, express.js, mongodb, postman, postgressSQL, and SQL. Along with experince useing java, pthon , jyupiterLabs, and R from past projects.
             </p>
-            <p>I'm not a designer but have experience incorporating designs in order to better display projects. For more examples of my skills I recommend you take a look at my work thats on github the link is down below </p>
+            <p>I'm not a designer but have experience incorporating designs in order to better display projects. For more examples of my skills I recommend you take a look at my work thats on github the link is down below. </p>
             <a href="https://github.com/jmctheowl8">LINK TO GITHUB</a>
         </div>
     );
